3695d47cfb783f355f1f8048ab2257bc7202a49a
2023-026 EomJiHee <jheom@ecounterp.co.kr>
2026-07-03 09:26:24 +0900

    Revert "Update asset references in index.html and replace obsolete JavaScript files"
    This reverts commit 15099edc1f296ab3843e64c42898a75f450c3aab.

--- assets/client-BTgqJNcL.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-Bk1xgNJ8.js";var t,e=o;t=e.createRoot,e.hydrateRoot;const a=Object.freeze(Object.defineProperty({__proto__:null,get createRoot(){return t}},Symbol.toStringTag,{value:"Module"}));export{a as c};
